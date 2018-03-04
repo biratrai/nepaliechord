61c244aba5fd4053ab89c97016b0843c8f795ba5
Birat Rai <biratrai@hotmail.com>
2018-03-04 15:48:50 -0500

    removing the junk and adding logs

--- app/src/main/java/com/example/gooner10/nepaliechord/mainsong/MainActivity.kt
+++ app/src/main/java/com/example/gooner10/nepaliechord/mainsong/MainActivity.kt
@@ -4,39 +4,37 @@ import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.FragmentPagerAdapter
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import com.example.gooner10.nepaliechord.R
 import com.example.gooner10.nepaliechord.model.Song
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), MainSongContract.MainSongView {
+    private val TAG = MainActivity::class.java.simpleName
     private var adapterViewPager: FragmentPagerAdapter? = null
+    private var presenter: MainSongActivityPresenter = MainSongActivityPresenter(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         adapterViewPager = MainActivityViewPagerAdapter(supportFragmentManager)
         viewPager.adapter = adapterViewPager
+        presenter.fetchSong()
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 
-    override fun displaySong(): MutableList<Song> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun displaySong(songList: MutableList<Song>) {
+        Log.d(TAG, "songlist: " + songList)
     }
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-//                message.setText(R.string.title_home)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_favorite -> {
-//                message.setText(R.string.title_dashboard)
                 return@OnNavigationItemSelectedListener true
             }
-//            R.id.navigation_notifications -> {
-//                message.setText(R.string.title_notifications)
-//                return@OnNavigationItemSelectedListener true
-//            }
         }
         false
     }
