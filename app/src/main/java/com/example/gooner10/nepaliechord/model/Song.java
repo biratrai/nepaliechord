04e20a113e85005adb64219833903368bc68e12d
Birat Rai <biratrai@hotmail.com>
2018-03-04 15:16:40 -0500

    adding song model

---
+++ app/src/main/java/com/example/gooner10/nepaliechord/model/Song.java
@@ -0,0 +1,12 @@
+package com.example.gooner10.nepaliechord.model;
+
+/**
+ * Song pojo
+ */
+
+public class Song {
+    String id;
+    String artistName;
+    String lyrics;
+    boolean isFavorite;
+}
