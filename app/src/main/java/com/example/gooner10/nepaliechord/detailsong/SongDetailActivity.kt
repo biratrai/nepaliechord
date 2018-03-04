49daf0616ff03e0759a17aa084030cd34efc1c0d
Birat Rai <biratrai@hotmail.com>
2018-03-04 15:16:33 -0500

    adding song detail actiivty

---
+++ app/src/main/java/com/example/gooner10/nepaliechord/detailsong/SongDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.example.gooner10.nepaliechord.detailsong
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.gooner10.nepaliechord.R
+
+class SongDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_song_detail)
+    }
+}
