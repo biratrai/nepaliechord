e4f1d104a61ada0eb04eed76dda772a0d3b45c4c
Birat Rai <biratrai@hotmail.com>
2018-03-04 15:49:07 -0500

    adding logic to send static data

--- app/src/main/java/com/example/gooner10/nepaliechord/mainsong/MainSongActivityPresenter.java
+++ app/src/main/java/com/example/gooner10/nepaliechord/mainsong/MainSongActivityPresenter.java
@@ -3,6 +3,7 @@ package com.example.gooner10.nepaliechord.mainsong;
 import com.example.gooner10.nepaliechord.mainsong.MainSongContract.MainSongPresenter;
 import com.example.gooner10.nepaliechord.model.Song;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -10,8 +11,20 @@ import java.util.List;
  */
 
 public class MainSongActivityPresenter implements MainSongPresenter {
+    private MainSongContract.MainSongView songView;
+
+    public MainSongActivityPresenter(MainSongContract.MainSongView songView) {
+        this.songView = songView;
+    }
+
     @Override
-    public List<Song> fetchSong() {
-        return null;
+    public void fetchSong() {
+
+        List<Song> songList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Song song = new Song(i + ") Sugam Pokhrel", i + ": Mero Sansar", true);
+            songList.add(song);
+        }
+        songView.displaySong(songList);
     }
 }
