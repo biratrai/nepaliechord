d0378e91d716e050f8de2b1737557e14c510318f
Birat Rai <biratrai@hotmail.com>
2018-03-04 15:48:15 -0500

    converting to kotlin pojo

---
+++ app/src/main/java/com/example/gooner10/nepaliechord/model/Song.kt
@@ -0,0 +1,9 @@
+package com.example.gooner10.nepaliechord.model
+
+/**
+ * Song pojo
+ */
+
+class Song(var artistName: String?, var lyrics: String?, var isFavorite: Boolean) {
+    var id: String? = null
+}
