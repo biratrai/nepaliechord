eb1fc9a7ccea470c8ac1246d43d22553ef2e9da6
Birat Rai <biratrai@hotmail.com>
2018-03-04 17:48:19 -0500

    using the SmartFragmentStatePagerAdapter

--- app/src/main/java/com/example/gooner10/nepaliechord/mainsong/MainActivity.kt
+++ app/src/main/java/com/example/gooner10/nepaliechord/mainsong/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.gooner10.nepaliechord.mainsong
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
-import android.support.v4.app.FragmentPagerAdapter
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import com.example.gooner10.nepaliechord.R
@@ -11,20 +10,22 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), MainSongContract.MainSongView {
     private val TAG = MainActivity::class.java.simpleName
-    private var adapterViewPager: FragmentPagerAdapter? = null
+    private var pagerAdapter: SmartFragmentStatePagerAdapter? = null
     private var presenter: MainSongActivityPresenter = MainSongActivityPresenter(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        adapterViewPager = MainActivityViewPagerAdapter(supportFragmentManager)
-        viewPager.adapter = adapterViewPager
+        pagerAdapter = MainActivityViewPagerAdapter(supportFragmentManager)
+        viewPager.adapter = pagerAdapter
         presenter.fetchSong()
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 
     override fun displaySong(songList: MutableList<Song>) {
         Log.d(TAG, "songlist: " + songList)
+        Log.d(TAG, "currentItem: " + viewPager.currentItem)
+        Log.d(TAG, "current fragment " + pagerAdapter?.getRegisteredFragment(viewPager.currentItem));
     }
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
