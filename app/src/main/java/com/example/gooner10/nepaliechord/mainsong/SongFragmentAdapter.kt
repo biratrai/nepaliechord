0a0f099cbbb72c769c946a9d9ca6d9d8f56b8329
Birat Rai <biratrai@hotmail.com>
2018-04-16 21:24:24 -0400

    using the folder syntax

--- app/src/main/java/com/example/gooner10/nepaliechord/mainsong/SongFragmentAdapter.kt
+++ app/src/main/java/com/example/gooner10/nepaliechord/mainsong/SongFragmentAdapter.kt
@@ -29,11 +29,7 @@ class SongFragmentAdapter(private var mValues: ArrayList<Song>, private val mLis
         holder.mContentView.text = mValues[position].lyrics
 
         holder.mView.setOnClickListener {
-            if (null != mListener) {
-                // Notify the active callbacks interface (the activity, if the
-                // fragment is attached to one) that an item has been selected.
-                mListener.onListFragmentInteraction(holder.mItem!!)
-            }
+            mListener?.onListFragmentInteraction(holder.mItem!!)
         }
     }
 
