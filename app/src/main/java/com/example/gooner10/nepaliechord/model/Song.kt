656002f0bac3c4fe7fc2a2fec5a1ce5998c53332
Birat Rai <biratrai@hotmail.com>
2018-04-18 21:47:20 -0400

    cleaning the Song class

--- app/src/main/java/com/example/gooner10/nepaliechord/model/Song.kt
+++ app/src/main/java/com/example/gooner10/nepaliechord/model/Song.kt
@@ -4,8 +4,7 @@ package com.example.gooner10.nepaliechord.model
  * Song pojo
  */
 
-class Song(var artistName: String?,
-           var lyrics: String?,
+class Song(var artistName: String,
+           var songTitle: String,
            var isFavorite: Boolean,
-           var rating: Int) {
-}
+           var rating: Int)
