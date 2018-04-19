c741518bf8960e3ec22bbf5b612b38ea37efe67b
Birat Rai <biratrai@hotmail.com>
2018-04-18 22:10:34 -0400

    implementing the parcelable

--- app/src/main/java/com/example/gooner10/nepaliechord/model/Song.kt
+++ app/src/main/java/com/example/gooner10/nepaliechord/model/Song.kt
@@ -1,5 +1,8 @@
 package com.example.gooner10.nepaliechord.model
 
+import android.os.Parcel
+import android.os.Parcelable
+
 /**
  * Song pojo
  */
@@ -7,4 +10,32 @@ package com.example.gooner10.nepaliechord.model
 class Song(var artistName: String,
            var songTitle: String,
            var isFavorite: Boolean,
-           var rating: Int)
+           var rating: Int) : Parcelable {
+
+    constructor(parcel: Parcel) : this(
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readByte() != 0.toByte(),
+            parcel.readInt())
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(artistName)
+        parcel.writeString(songTitle)
+        parcel.writeByte(if (isFavorite) 1 else 0)
+        parcel.writeInt(rating)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Song> {
+        override fun createFromParcel(parcel: Parcel): Song {
+            return Song(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Song?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
