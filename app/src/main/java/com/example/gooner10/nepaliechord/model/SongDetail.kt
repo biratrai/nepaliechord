a5d1834556e6d544f7489a51dbeb9a26153e05e1
Birat Rai <biratrai@hotmail.com>
2018-04-18 21:47:09 -0400

    adding songDetail class

---
+++ app/src/main/java/com/example/gooner10/nepaliechord/model/SongDetail.kt
@@ -0,0 +1,7 @@
+package com.example.gooner10.nepaliechord.model
+
+class SongDetail(var artistName: String,
+                 var songTitle: String,
+                 var isFavorite: Boolean,
+                 var rating: Int,
+                 var lyrics: String)
