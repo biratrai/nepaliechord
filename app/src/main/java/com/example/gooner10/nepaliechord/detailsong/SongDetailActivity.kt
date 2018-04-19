175a96280038e07e83ea1334a399ef77c37bbea1
Birat Rai <biratrai@hotmail.com>
2018-04-18 22:09:45 -0400

    receiving the intent and setting the title

--- app/src/main/java/com/example/gooner10/nepaliechord/detailsong/SongDetailActivity.kt
+++ app/src/main/java/com/example/gooner10/nepaliechord/detailsong/SongDetailActivity.kt
@@ -3,11 +3,16 @@ package com.example.gooner10.nepaliechord.detailsong
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.example.gooner10.nepaliechord.R
+import com.example.gooner10.nepaliechord.model.Song
+import kotlinx.android.synthetic.main.activity_song_detail.*
 
 class SongDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_song_detail)
+
+        val song = intent.getParcelableExtra<Song>("Song")
+        songTitleName.text = song.songTitle
     }
 }
