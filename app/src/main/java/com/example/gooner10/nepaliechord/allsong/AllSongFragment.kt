cc8e034fb96e26aac24cb2494abfd7d3ca8a03b3
Birat Rai <biratrai@hotmail.com>
2018-06-16 10:45:21 -0400

    mvoing to all song package and remaing adapter

---
+++ app/src/main/java/com/example/gooner10/nepaliechord/allsong/AllSongFragment.kt
@@ -0,0 +1,95 @@
+package com.example.gooner10.nepaliechord.allsong
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.gooner10.nepaliechord.R
+import com.example.gooner10.nepaliechord.BaseFragment
+import com.example.gooner10.nepaliechord.model.Song
+
+/**
+ * A fragment representing a list of Items.
+ *
+ *
+ * Activities containing this fragment MUST implement the [OnListFragmentInteractionListener]
+ * interface.
+ */
+/**
+ * Mandatory empty constructor for the fragment manager to instantiate the
+ * fragment (e.g. upon screen orientation changes).
+ */
+class AllSongFragment : BaseFragment() {
+    private var listener: OnAllSongFragmentItemListener? = null
+    private val songList = arrayListOf<Song>()
+    private var adapterAll: AllSongAdapter? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.getString(SONG_TITLE)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.all_song_fragment, container, false)
+
+        // Set the adapterAll
+        if (view is RecyclerView) {
+            val context = view.getContext()
+            view.layoutManager = LinearLayoutManager(context)
+            view.adapter = AllSongAdapter(songList, listener)
+            val dividerItemDecoration = DividerItemDecoration(getContext(), (view.layoutManager as LinearLayoutManager).orientation)
+            view.addItemDecoration(dividerItemDecoration)
+            adapterAll = view.adapter as AllSongAdapter
+        }
+        return view
+    }
+
+    override fun setData(songList: List<Song>) {
+        Log.d(TAG, "song data received")
+        adapterAll?.setData(songList)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        if (context is OnAllSongFragmentItemListener) {
+            listener = context
+        } else {
+            throw RuntimeException(context.toString() + " must implement OnAllSongFragmentItemListener")
+        }
+    }
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     *
+     *
+     * See the Android Training lesson [Communicating with Other Fragments](http://developer.android.com/training/basics/fragments/communicating.html) for more information.
+     */
+    interface OnAllSongFragmentItemListener {
+        fun onListFragmentInteraction(song: Song)
+    }
+
+    companion object {
+        private const val SONG_TITLE = "song-title"
+        private val TAG = AllSongFragment::class.java.simpleName
+
+        fun newInstance(title: String): AllSongFragment {
+            val fragment = AllSongFragment()
+            val args = Bundle()
+            args.putString(SONG_TITLE, title)
+            fragment.arguments = args
+            return fragment
+        }
+    }
+}
