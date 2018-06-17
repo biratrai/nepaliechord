23d38edc622e38069035bafc7ee46cbd07c275bb
Birat Rai <biratrai@hotmail.com>
2018-06-17 13:34:28 -0400

    adding logic for click in icon

--- app/src/main/java/com/example/gooner10/nepaliechord/detailsong/SongDetailActivity.kt
+++ app/src/main/java/com/example/gooner10/nepaliechord/detailsong/SongDetailActivity.kt
@@ -2,6 +2,8 @@ package com.example.gooner10.nepaliechord.detailsong
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.View
 import com.example.gooner10.nepaliechord.R
 import com.example.gooner10.nepaliechord.model.Song
 import kotlinx.android.synthetic.main.activity_song_detail.*
@@ -16,5 +18,21 @@ class SongDetailActivity : AppCompatActivity() {
         songTitleName.text = song.songTitle
         webView.settings.javaScriptEnabled = true
         webView.loadUrl("file:///android_asset/song.html")
+
+        favorite_icon_detail.setOnClickListener { v: View? ->
+
+            Log.i(TAG, "Clicked "+ song.isFavorite)
+            if (song.isFavorite) {
+                favorite_icon_detail.setBackground(getDrawable(R.drawable.ic_favorite_border_black_24dp))
+                song.isFavorite = false
+            } else {
+                song.isFavorite = true
+                favorite_icon_detail.setBackground(getDrawable(R.drawable.ic_favorite_black_24dp))
+            }
+        }
+    }
+
+    companion object {
+        private val TAG = SongDetailActivity::class.java.simpleName
     }
 }
