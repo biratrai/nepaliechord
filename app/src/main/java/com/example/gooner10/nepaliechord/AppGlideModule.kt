7fc5aa732a0109ff6364704185b4c95a98191e71
Birat Rai <biratrai@hotmail.com>
2018-06-18 22:14:29 -0400

    adding glideModule for kotlin

---
+++ app/src/main/java/com/example/gooner10/nepaliechord/AppGlideModule.kt
@@ -0,0 +1,7 @@
+package com.example.gooner10.nepaliechord
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class AppGlideModule : AppGlideModule()
