6c2a9f0fa5559534925dfbd36cec0fc4b022ccc3
Birat Rai <biratrai@hotmail.com>
2018-06-19 22:38:27 -0400

    removing unnecessary model

--- app/src/main/java/com/example/gooner10/nepaliechord/model/SongDetail.kt
+++
@@ -1,7 +0,0 @@
-package com.example.gooner10.nepaliechord.model
-
-class SongDetail(var artistName: String,
-                 var songTitle: String,
-                 var isFavorite: Boolean,
-                 var rating: Int,
-                 var lyrics: String)
