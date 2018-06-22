a72e4bb5b2ea1d9b94592deebbfe0655b41df9a0
Birat Rai <biratrai@hotmail.com>
2018-06-21 22:13:50 -0400

    adding RecentSongFragment and moving constant to companion object

--- app/src/main/java/com/example/gooner10/nepaliechord/mainsong/MainActivityViewPagerAdapter.kt
+++ app/src/main/java/com/example/gooner10/nepaliechord/mainsong/MainActivityViewPagerAdapter.kt
@@ -5,18 +5,19 @@ import android.support.v4.app.FragmentManager
 import android.util.Log
 import com.example.gooner10.nepaliechord.allsong.AllSongFragment
 import com.example.gooner10.nepaliechord.favoritesong.FavoriteSongFragment
+import com.example.gooner10.nepaliechord.recentsong.RecentSongFragment
 
 /**
  * Pager Adapter for ViewPager in [MainActivity]
  */
 
 class MainActivityViewPagerAdapter(fm: FragmentManager) : SmartFragmentStatePagerAdapter(fm) {
-    private val NUM_ITEMS = 2
-    private val TAG = MainActivityViewPagerAdapter::class.java.simpleName
+
     override fun getItem(position: Int): Fragment? {
         when (position) {
             0 -> return AllSongFragment.newInstance("Songs")
             1 -> return FavoriteSongFragment.newInstance()
+            2 -> return RecentSongFragment.newInstance()
             else ->
                 Log.e(TAG, "Not defined")
         }
@@ -28,6 +29,16 @@ class MainActivityViewPagerAdapter(fm: FragmentManager) : SmartFragmentStatePage
     }
 
     override fun getPageTitle(position: Int): CharSequence {
-        return if (position == 0) "Songs" else "Favorite"
+        return when (position) {
+            0 -> "Songs"
+            1 -> "Favorite"
+            else -> "Recent"
+        }
+    }
+
+    // Used to define the constant
+    companion object {
+        private const val NUM_ITEMS = 3
+        private val TAG = MainActivityViewPagerAdapter::class.java.simpleName
     }
 }
