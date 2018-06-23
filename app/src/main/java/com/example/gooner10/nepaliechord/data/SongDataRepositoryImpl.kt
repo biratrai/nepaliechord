d96e4ea25d8c601e465f40d4e3ef1ec1eecec828
Birat Rai <biratrai@hotmail.com>
2018-06-23 11:13:54 -0400

    adding implementation of the SondDataRepository

---
+++ app/src/main/java/com/example/gooner10/nepaliechord/data/SongDataRepositoryImpl.kt
@@ -0,0 +1,15 @@
+package com.example.gooner10.nepaliechord.data
+
+class SongDataRepositoryImpl : SongDataRepository{
+    override fun getAllSong() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getFavoriteSong() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getRecentSong() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
