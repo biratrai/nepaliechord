d52f04a2cdd6fbb0bdb2bf76a40b41f6491686de
Birat Rai <biratrai@hotmail.com>
2018-11-12 06:03:44 -0500

    chanigng the contract to return List of Song and using it to retrive the song in MainSongActivityPresenter

--- app/src/main/java/com/example/gooner10/nepaliechord/data/SongDataRepository.kt
+++ app/src/main/java/com/example/gooner10/nepaliechord/data/SongDataRepository.kt
@@ -1,7 +1,10 @@
 package com.example.gooner10.nepaliechord.data
 
+import com.example.gooner10.nepaliechord.model.Song
+import java.util.ArrayList
+
 interface SongDataRepository {
-    fun getAllSong()
+    fun getAllSong(): List<Song>
     fun getFavoriteSong()
     fun getRecentSong()
 }
--- app/src/main/java/com/example/gooner10/nepaliechord/mainsong/MainSongActivityPresenter.kt
+++ app/src/main/java/com/example/gooner10/nepaliechord/mainsong/MainSongActivityPresenter.kt
@@ -1,27 +1,19 @@
 package com.example.gooner10.nepaliechord.mainsong
 
+import com.example.gooner10.nepaliechord.data.SongDataRepositoryImpl
 import com.example.gooner10.nepaliechord.mainsong.MainSongContract.MainSongPresenter
-import com.example.gooner10.nepaliechord.model.Song
-import java.util.*
 
 /**
- * Presenter for [MainActivity]
+ * Presenter for [MainSongActivity]
  */
 
 class MainSongActivityPresenter(private val songView: MainSongContract.MainSongView) : MainSongPresenter {
 
+    private val songDataRepository: SongDataRepositoryImpl = SongDataRepositoryImpl()
+    
     override fun fetchSong() {
 
-        val songList = ArrayList<Song>()
-        for (i in 1..9) {
-            val song = Song(i.toString() + ") Sugam Pokhrel",
-                    ": Mero Sansar",
-                    true,
-                    1 + i,
-                    "file:///android_asset/song.html",
-                    System.currentTimeMillis())
-            songList.add(song)
-        }
+        val songList = songDataRepository.getAllSong()
         songView.displaySong(songList)
     }
 }
