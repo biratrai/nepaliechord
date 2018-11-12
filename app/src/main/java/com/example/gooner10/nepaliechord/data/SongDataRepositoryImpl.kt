187b3545db203b984148b0849e94a1355bfcaf84
Birat Rai <biratrai@hotmail.com>
2018-11-12 06:03:59 -0500

    adding implementation to getAllSong

--- app/src/main/java/com/example/gooner10/nepaliechord/data/SongDataRepositoryImpl.kt
+++ app/src/main/java/com/example/gooner10/nepaliechord/data/SongDataRepositoryImpl.kt
@@ -1,12 +1,24 @@
 package com.example.gooner10.nepaliechord.data
 
+import com.example.gooner10.nepaliechord.model.Song
 import com.google.firebase.database.FirebaseDatabase
+import java.util.ArrayList
 
 class SongDataRepositoryImpl : SongDataRepository {
     val database = FirebaseDatabase.getInstance()
 
-    override fun getAllSong() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun getAllSong(): List<Song> {
+        val songList = ArrayList<Song>()
+        for (i in 1..9) {
+            val song = Song(i.toString() + ") Sugam Pokhrel",
+                    ": Mero Sansar",
+                    true,
+                    1 + i,
+                    "file:///android_asset/song.html",
+                    System.currentTimeMillis())
+            songList.add(song)
+        }
+        return songList
     }
 
     override fun getFavoriteSong() {
