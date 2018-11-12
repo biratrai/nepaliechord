262bd60f04bf8f1b3ed10e6b6d967a46dc314755
Birat Rai <biratrai@hotmail.com>
2018-11-12 06:44:50 -0500

    deleting the google-services.json and adding call to the firebaseDatabase

--- app/src/main/java/com/example/gooner10/nepaliechord/data/SongDataRepositoryImpl.kt
+++ app/src/main/java/com/example/gooner10/nepaliechord/data/SongDataRepositoryImpl.kt
@@ -1,14 +1,31 @@
 package com.example.gooner10.nepaliechord.data
 
 import com.example.gooner10.nepaliechord.model.Song
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
 import java.util.ArrayList
 
-class SongDataRepositoryImpl : SongDataRepository {
-    val database = FirebaseDatabase.getInstance()
+class SongDataRepositoryImpl : SongDataRepository, AnkoLogger {
+    private val database = FirebaseDatabase.getInstance()
 
     override fun getAllSong(): List<Song> {
         val songList = ArrayList<Song>()
+        val databaseRef = database.getReference("song")
+        info("getAllSong")
+        databaseRef.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+                info("onCancelled "+ p0.message)
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                info("onDataChange "+ p0)
+            }
+
+        })
         for (i in 1..9) {
             val song = Song(i.toString() + ") Sugam Pokhrel",
                     ": Mero Sansar",
