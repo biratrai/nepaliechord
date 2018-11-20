e2f4b7c03b7c9879d68a22e1e30b7cac68524858
Birat Rai <biratrai@hotmail.com>
2018-11-20 17:04:20 -0500

    adding toolbar and image

--- app/src/main/java/com/example/gooner10/nepaliechord/artistsong/ArtistSongActivity.kt
+++ app/src/main/java/com/example/gooner10/nepaliechord/artistsong/ArtistSongActivity.kt
@@ -1,8 +1,11 @@
 package com.example.gooner10.nepaliechord.artistsong
 
+import android.content.Context
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
+import com.bumptech.glide.request.RequestOptions
+import com.example.gooner10.nepaliechord.GlideApp
 import com.example.gooner10.nepaliechord.R
 import com.example.gooner10.nepaliechord.model.SingerDetail
 
@@ -17,6 +20,10 @@ class ArtistSongActivity : AppCompatActivity() {
 
         val singer: SingerDetail = intent.getParcelableExtra("Singer")
         collapsing_toolbar.title = singer.singerName
+
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        GlideApp.with(this).load(singer.singerPhoto).into(backdrop)
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
