dd876737775c1f0aeada481b3bc9a6c121773543
Birat Rai <biratrai@hotmail.com>
2018-11-19 20:12:43 -0500

    adding ChordApplication to the Mainfest and creating the base class

---
+++ app/src/main/java/com/example/gooner10/nepaliechord/ChordApplication.kt
@@ -0,0 +1,13 @@
+package com.example.gooner10.nepaliechord
+
+import android.app.Application
+import com.google.firebase.auth.FirebaseAuth
+
+class ChordApplication : Application() {
+//    val firebase: FirebaseAuth = FirebaseAuth.getInstance()
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
