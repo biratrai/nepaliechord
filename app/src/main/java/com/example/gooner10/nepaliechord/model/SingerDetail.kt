472c855c8b3c9807305194b1c05f20092d9b71a9
Birat Rai <biratrai@hotmail.com>
2018-11-20 16:39:08 -0500

    implementing parcelable

--- app/src/main/java/com/example/gooner10/nepaliechord/model/SingerDetail.kt
+++ app/src/main/java/com/example/gooner10/nepaliechord/model/SingerDetail.kt
@@ -1,9 +1,18 @@
 package com.example.gooner10.nepaliechord.model
 
-class SingerDetail() {
+import android.os.Parcel
+import android.os.Parcelable
+
+class SingerDetail() : Parcelable {
     var singerId: String? = null
     var singerName: String? = null
     var singerPhoto: String? = null
+
+    constructor(parcel: Parcel) : this() {
+        singerId = parcel.readString()
+        singerName = parcel.readString()
+        singerPhoto = parcel.readString()
+    }
 
     constructor(singerId: String
                 , singerName: String
@@ -11,5 +20,25 @@ class SingerDetail() {
         this.singerId = singerId
         this.singerName = singerName
         this.singerPhoto = singerPhoto
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(singerId)
+        parcel.writeString(singerName)
+        parcel.writeString(singerPhoto)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<SingerDetail> {
+        override fun createFromParcel(parcel: Parcel): SingerDetail {
+            return SingerDetail(parcel)
+        }
+
+        override fun newArray(size: Int): Array<SingerDetail?> {
+            return arrayOfNulls(size)
+        }
     }
 }
