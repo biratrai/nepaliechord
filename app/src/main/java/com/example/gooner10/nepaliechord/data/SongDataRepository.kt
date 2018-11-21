18402be2fb95ea314335dbb63cfd57d36d3d14e1
Birat Rai <biratrai@hotmail.com>
2018-11-20 23:23:09 -0500

    fetching songByArtist

--- app/src/main/java/com/example/gooner10/nepaliechord/data/SongDataRepository.kt
+++ app/src/main/java/com/example/gooner10/nepaliechord/data/SongDataRepository.kt
@@ -1,5 +1,6 @@
 package com.example.gooner10.nepaliechord.data
 
+import com.example.gooner10.nepaliechord.artistsong.ArtistSongActivityPresenter
 import com.example.gooner10.nepaliechord.mainsong.MainSongActivityPresenter
 import com.example.gooner10.nepaliechord.model.Song
 
@@ -7,5 +8,5 @@ interface SongDataRepository {
     fun getAllArtist(mainSongActivityPresenter: MainSongActivityPresenter)
     fun getFavoriteSong(): List<Song>
     fun getRecentSong(): List<Song>
-    fun getSongByArtist(singerId: String): List<Song>
+    fun getSongByArtist(singerId: String, view: ArtistSongActivityPresenter)
 }
