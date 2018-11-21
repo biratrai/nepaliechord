7d07d028e2299dc4df4839fbdce79213653eb18d
Birat Rai <biratrai@hotmail.com>
2018-11-20 23:22:16 -0500

    constant for firebase

---
+++ app/src/main/java/com/example/gooner10/nepaliechord/NepaliChordConstant.kt
@@ -0,0 +1,10 @@
+package com.example.gooner10.nepaliechord
+
+class NepaliChordConstant {
+    companion object {
+        const val ALL_SONGS = "allSongs"
+        const val ALL_SINGERS = "singers"
+        const val ALL_FAVORITE = "favoriteSongs"
+        const val ALL_RECENT = "recentSongs"
+    }
+}
