dd17a026e195090e254014a2a9e379456cadb6e8
Birat Rai <biratrai@hotmail.com>
2018-11-21 12:19:50 -0500

    applying graytranfsorm scale

--- app/src/main/java/com/example/gooner10/nepaliechord/artistsong/ArtistSongActivity.kt
+++ app/src/main/java/com/example/gooner10/nepaliechord/artistsong/ArtistSongActivity.kt
@@ -1,14 +1,19 @@
 package com.example.gooner10.nepaliechord.artistsong
 
+import android.os.Build
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
+import android.view.MenuItem
+import com.bumptech.glide.load.MultiTransformation
+import com.bumptech.glide.load.resource.bitmap.FitCenter
+import com.bumptech.glide.request.RequestOptions
 import com.example.gooner10.nepaliechord.GlideApp
 import com.example.gooner10.nepaliechord.R
 import com.example.gooner10.nepaliechord.model.SingerDetail
 import com.example.gooner10.nepaliechord.model.Song
+import jp.wasabeef.glide.transformations.GrayscaleTransformation
 import kotlinx.android.synthetic.main.activity_artist_song.*
 import kotlinx.android.synthetic.main.content_artist_song.*
 import org.jetbrains.anko.AnkoLogger
@@ -22,20 +27,17 @@ class ArtistSongActivity : AppCompatActivity(), ArtistSongContract.ArtistSongVie
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_artist_song)
-        setSupportActionBar(toolbar)
 
         val singer: SingerDetail = intent.getParcelableExtra("Singer")
         presenter.fetchArtistSong(singer.singerId!!)
 
         collapsing_toolbar.title = singer.singerName
 
-        setSupportActionBar(toolbar)
+        setSupportActionBar(artistActivityToolbar)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-        GlideApp.with(this).load(singer.singerPhoto).fitCenter().into(backdrop)
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+
+        val multi = MultiTransformation(GrayscaleTransformation(), FitCenter())
+        GlideApp.with(this).load(singer.singerPhoto).apply(RequestOptions.bitmapTransform(multi)).into(backdrop)
 
         adapterAll = ArtistSongAdapter(songList, this)
         artistSongRecyclerView.adapter = adapterAll
