70bae01e15382128cead4be62848fe48a4100229
Birat Rai <biratrai@hotmail.com>
2018-11-22 15:05:09 -0500

    removing unnecessary lines

--- app/src/main/java/com/example/gooner10/nepaliechord/detailsong/SongDetailActivity.kt
+++ app/src/main/java/com/example/gooner10/nepaliechord/detailsong/SongDetailActivity.kt
@@ -12,7 +12,6 @@ import com.example.gooner10.nepaliechord.model.SongDetail
 import jp.wasabeef.glide.transformations.RoundedCornersTransformation
 import kotlinx.android.synthetic.main.activity_song_detail.*
 import org.jetbrains.anko.AnkoLogger
-import org.jetbrains.anko.info
 
 class SongDetailActivity : AppCompatActivity(), SongDetailContract.SongDetailView, AnkoLogger {
     private var presenter: SongDetailPresenter = SongDetailPresenter(this)
@@ -22,16 +21,12 @@ class SongDetailActivity : AppCompatActivity(), SongDetailContract.SongDetailVie
         setContentView(R.layout.activity_song_detail)
 
         val song: Song = intent.extras.get("SongDetail") as Song
-        info("song ${song.songId}")
-        info("song ${song.songTitle}")
         songTitleName.text = song.songTitle
         webView.settings.javaScriptEnabled = true
         setSupportActionBar(activitySongToolbar)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
         presenter.fetchSongDetail(song.songId!!)
-//        webView.loadUrl("file:///android_asset/song.html")
-//        webView.loadDataWithBaseURL("", getString(R.string.html), "text/html", "utf-8", "")
 
         GlideApp.with(this).load(R.drawable.ic_account_circle_black_24dp)
                 .placeholder(R.drawable.ic_account_circle_black_24dp)
@@ -62,13 +57,9 @@ class SongDetailActivity : AppCompatActivity(), SongDetailContract.SongDetailVie
         }
     }
 
-    companion object {
-        private val TAG = SongDetailActivity::class.java.simpleName
-    }
-
     override fun displaySongDetail(songDetail: SongDetail) {
         if (songDetail.songLyrics != "")
-            webView.loadUrl(songDetail.songLyrics)
+            webView.loadDataWithBaseURL("", songDetail.songLyrics, "text/html", "utf-8", "")
         else
             webView.loadUrl("file:///android_asset/song.html")
     }
