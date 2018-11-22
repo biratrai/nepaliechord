9c3d9e0ac6a5683162b9087f06eb48dafcb3a862
Birat Rai <biratrai@hotmail.com>
2018-11-22 14:47:37 -0500

    adding method to getSongDetailById

--- app/src/main/java/com/example/gooner10/nepaliechord/data/SongDataRepository.kt
+++ app/src/main/java/com/example/gooner10/nepaliechord/data/SongDataRepository.kt
@@ -1,6 +1,7 @@
 package com.example.gooner10.nepaliechord.data
 
 import com.example.gooner10.nepaliechord.artistsong.ArtistSongActivityPresenter
+import com.example.gooner10.nepaliechord.detailsong.SongDetailContract
 import com.example.gooner10.nepaliechord.mainsong.MainSongActivityPresenter
 import com.example.gooner10.nepaliechord.model.Song
 
@@ -9,4 +10,5 @@ interface SongDataRepository {
     fun getFavoriteSong(): List<Song>
     fun getRecentSong(): List<Song>
     fun getSongByArtist(singerId: String, view: ArtistSongActivityPresenter)
+    fun getSongDetailById(songId: String, view: SongDetailContract.SongDetailPresenter)
 }
