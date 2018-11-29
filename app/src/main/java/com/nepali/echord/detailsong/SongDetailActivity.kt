03311b048b3aefff909ddec9fa82d05601fa8847
Birat Rai <biratrai@hotmail.com>
2018-11-28 22:31:36 -0500

    Styling the bottom sheet

--- app/src/main/java/com/nepali/echord/detailsong/SongDetailActivity.kt
+++ app/src/main/java/com/nepali/echord/detailsong/SongDetailActivity.kt
@@ -1,17 +1,15 @@
 package com.nepali.echord.detailsong
 
+import android.graphics.Color
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v4.content.res.ResourcesCompat
 import android.support.v7.app.AppCompatActivity
-import com.bumptech.glide.request.RequestOptions
-import com.nepali.echord.GlideApp
 import com.nepali.echord.NepaliChordConstant.Companion.SONG_DETAIL_INTENT
 import com.nepali.echord.R
 import com.nepali.echord.model.Song
 import com.nepali.echord.model.SongDetail
-import jp.wasabeef.glide.transformations.RoundedCornersTransformation
-import kotlinx.android.synthetic.main.activity_song_detail.*
+import kotlinx.android.synthetic.main.app_tool_bar.*
+import kotlinx.android.synthetic.main.song_detail_bottom_sheet.*
+import kotlinx.android.synthetic.main.song_detail_main_content.*
 import org.jetbrains.anko.AnkoLogger
 
 class SongDetailActivity : AppCompatActivity(), SongDetailContract.SongDetailView, AnkoLogger {
@@ -24,16 +22,17 @@ class SongDetailActivity : AppCompatActivity(), SongDetailContract.SongDetailVie
         val song: Song = intent.extras.get(SONG_DETAIL_INTENT) as Song
         songTitleName.text = song.songTitle
         webView.settings.javaScriptEnabled = true
-        setSupportActionBar(activitySongToolbar)
+        webView.setBackgroundColor(Color.TRANSPARENT)
+        setSupportActionBar(app_toolbar)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
         presenter.fetchSongDetail(song.songId!!)
 
-        GlideApp.with(this).load(R.drawable.ic_account_circle_black_24dp)
-                .placeholder(R.drawable.ic_account_circle_black_24dp)
-                .error(R.drawable.ic_account_circle_black_24dp)
-                .apply(RequestOptions.bitmapTransform(RoundedCornersTransformation(50, 2)))
-                .into(artist_image_icon_detail)
+//        GlideApp.with(this).load(R.drawable.ic_account_circle_black_24dp)
+//                .placeholder(R.drawable.ic_account_circle_black_24dp)
+//                .error(R.drawable.ic_account_circle_black_24dp)
+//                .apply(RequestOptions.bitmapTransform(RoundedCornersTransformation(50, 2)))
+//                .into(artist_image_icon_detail)
 
 //        favorite_icon_detail.setOnClickListener { v: View? ->
 //
@@ -41,21 +40,21 @@ class SongDetailActivity : AppCompatActivity(), SongDetailContract.SongDetailVie
 //            setSong(song)
 //        }
 
-        favoriteFabIcon.setOnClickListener { view ->
-            Snackbar.make(view, "This is my favorite", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-            setSong(song)
-        }
+//        favoriteFabIcon.setOnClickListener { view ->
+//            Snackbar.make(view, "This is my favorite", Snackbar.LENGTH_LONG)
+//                    .setAction("Action", null).show()
+//            setSong(song)
+//        }
     }
 
     private fun setSong(song: Song) {
-        if (song.isFavorite) {
-            favoriteFabIcon.background = ResourcesCompat.getDrawable(resources, R.drawable.ic_favorite_border_black_24dp, null)
-            song.isFavorite = false
-        } else {
-            song.isFavorite = true
-            favoriteFabIcon.background = ResourcesCompat.getDrawable(resources, R.drawable.ic_favorite_red_24dp, null)
-        }
+//        if (song.isFavorite) {
+//            favoriteFabIcon.background = ResourcesCompat.getDrawable(resources, R.drawable.ic_favorite_border_black_24dp, null)
+//            song.isFavorite = false
+//        } else {
+//            song.isFavorite = true
+//            favoriteFabIcon.background = ResourcesCompat.getDrawable(resources, R.drawable.ic_favorite_red_24dp, null)
+//        }
     }
 
     override fun displaySongDetail(songDetail: SongDetail) {
