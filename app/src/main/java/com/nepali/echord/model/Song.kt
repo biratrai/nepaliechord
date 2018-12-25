465393fc2d6a608a950f510728265a4e5676e32e
Birat Rai <biratrai@hotmail.com>
2018-12-25 11:23:02 -0500

    adding field for tab and chord

--- app/src/main/java/com/nepali/echord/model/Song.kt
+++ app/src/main/java/com/nepali/echord/model/Song.kt
@@ -15,6 +15,8 @@ class Song() : Parcelable {
     var singerId: String? = null
     var songId: String? = null
     var timeStamp: Long = 0
+    var tab: Boolean = false
+    var chord: Boolean = true
 
     constructor(parcel: Parcel) : this() {
         artistName = parcel.readString()
@@ -24,11 +26,15 @@ class Song() : Parcelable {
         singerId = parcel.readString()
         songId = parcel.readString()
         timeStamp = parcel.readLong()
+        tab = parcel.readByte() != 0.toByte()
+        chord = parcel.readByte() != 0.toByte()
     }
 
     constructor(artistName: String,
                 songTitle: String,
                 isFavorite: Boolean,
+                tab: Boolean,
+                chord: Boolean,
                 rating: Int,
                 singerId: String,
                 songId: String,
@@ -40,12 +46,16 @@ class Song() : Parcelable {
         this.singerId = singerId
         this.songId = songId
         this.timeStamp = timeStamp
+        this.tab = tab
+        this.chord = chord
     }
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
         parcel.writeString(artistName)
         parcel.writeString(songTitle)
         parcel.writeByte(if (isFavorite) 1 else 0)
+        parcel.writeByte(if (tab) 1 else 0)
+        parcel.writeByte(if (chord) 1 else 0)
         parcel.writeInt(rating)
         parcel.writeString(singerId)
         parcel.writeString(songId)
