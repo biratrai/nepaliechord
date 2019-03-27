5f7ea36bee9f219b5fa821e44ea2cfc37d9effe1
Birat Rai <biratrai@hotmail.com>
2019-03-26 22:54:36 -0400

    injecting the MainApplicationComponent

--- app/src/main/java/com/nepali/echord/ChordApplication.kt
+++ app/src/main/java/com/nepali/echord/ChordApplication.kt
@@ -1,19 +1,17 @@
 package com.nepali.echord
 
 import dagger.android.AndroidInjector
-import dagger.android.support.DaggerApplication
+import dagger.android.DaggerApplication
 
-
+/**
+ * We create a custom {@link Application} class that extends  {@link DaggerApplication}.
+ * We then override applicationInjector() which tells Dagger how to make our @Singleton Component
+ * We never have to call `component.inject(this)` as {@link DaggerApplication} will do that for us.
+ */
 class ChordApplication : DaggerApplication() {
+
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
-        return MainApplicationComponent.Builder
-    }
-
-
-//    val firebase: FirebaseAuth = FirebaseAuth.getInstance()
-
-    override fun onCreate() {
-        super.onCreate()
+        return DaggerMainApplicationComponent.builder().application(this).build()
     }
 
 }
