9f3ce95fd94cb472f374a9b03042c92b7bf24c40
Birat Rai <biratrai@hotmail.com>
2019-03-31 21:33:27 -0400

    adding RepositoryModule for different flavor

---
+++ app/src/demo/java/com/nepali/echord/data/RepositoryModule.kt
@@ -0,0 +1,11 @@
+package com.nepali.echord.data
+
+import dagger.Module
+
+/**
+ * This is used by Dagger to inject the required arguments into the {@link SongDataRepositoryImpl}.
+ */
+@Module
+abstract class RepositoryModule {
+
+}
