104c121a5209288348350494e48464224b7baaa4
Birat Rai <biratrai@hotmail.com>
2019-04-07 12:56:01 -0400

    providing the SongDataRepositoryImpl

--- app/src/full/java/com/nepali/echord/data/RepositoryModule.kt
+++ app/src/full/java/com/nepali/echord/data/RepositoryModule.kt
@@ -1,11 +1,15 @@
 package com.nepali.echord.data
 
 import dagger.Module
+import dagger.Provides
 
 /**
  * This is used by Dagger to inject the required arguments into the {@link SongDataRepositoryImpl}.
  */
 @Module
-abstract class RepositoryModule {
-
+class RepositoryModule {
+    @Provides
+    internal fun providesSongRepository(): SongDataRepository {
+        return SongDataRepositoryImpl()
+    }
 }
